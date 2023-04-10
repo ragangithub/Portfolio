bb287e6172acd0a92d751952fa5730d0ae39bb31
ragan <raganalemu3@gmail.com>
2023-04-10 20:40:18 +0300

    Update HTML CSS and JS file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('.menu');
 const closee = document.querySelector('.close');
 const navLinks = document.querySelector('.nav-links');
+
 menu.addEventListener('click', () => {
   navLinks.style.display = 'flex';
   closee.style.display = 'block';
@@ -8,4 +9,13 @@ menu.addEventListener('click', () => {
 closee.addEventListener('click', () => {
   navLinks.style.display = 'none';
   closee.style.display = 'none';
+});
+
+document.addEventListener('click', (e) => {
+  const target1 = e.target.closest('#work');
+  const target2 = e.target.closest('#about');
+  const target3 = e.target.closest('#contact');
+  if (target1 || target2 || target3) {
+    navLinks.style.display = 'none';
+  }
 });
