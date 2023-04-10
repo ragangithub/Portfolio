4612025c19da5a3b6cc5c95e1f3012c5bc493717
ragan <raganalemu3@gmail.com>
2023-04-10 15:41:39 +0300

    fix eslint

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const menu = document.querySelector(".menu");
-const closee = document.querySelector(".close");
-const navLinks = document.querySelector(".nav-links");
-menu.addEventListener("click", () => {
-  navLinks.style.display = "flex";
-  closee.style.display = "block";
+const menu = document.querySelector('.menu');
+const closee = document.querySelector('.close');
+const navLinks = document.querySelector('.nav-links');
+menu.addEventListener('click', () => {
+  navLinks.style.display = 'flex';
+  closee.style.display = 'block';
 });
-closee.addEventListener("click", () => {
-  navLinks.style.display = "none";
-  closee.style.display = "none";
+closee.addEventListener('click', () => {
+  navLinks.style.display = 'none';
+  closee.style.display = 'none';
 });
