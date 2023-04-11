bbd728c31e0ed1e0b7bbc63bcd929ee279df38d9
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-04-11 06:24:15 -0600

    Add JS pop

--- script.js
+++ script.js
@@ -19,3 +19,228 @@ document.addEventListener('click', (e) => {
     navLinks.style.display = 'none';
   }
 });
+
+
+const works = document.querySelector(".works");
+
+const header = document.querySelector(".header");
+const projects = [
+  {
+    image_mobile: "./images/Snapshoot-Portfolio.png",
+    image_desktop: "./images/nature.png",
+    name: "Tonic",
+    description:
+      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    details:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+
+    live: "https://ragangithub.github.io/",
+    source: "https://github.com/ragangithub/Microverse-Portfolio",
+  },
+  {
+    image_mobile: "./images/Availablity.png",
+    image_desktop: "./images/proffesional.png",
+    name: " Multi-Post Stories",
+    description:
+      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    details:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+
+    live: "https://ragangithub.github.io/",
+    source: "https://github.com/ragangithub/Microverse-Portfolio",
+  },
+  {
+    image_mobile: "./images/flex.png",
+    image_desktop: "./images/Snapshoot-Portfolio.png",
+    name: "Tonic",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    details:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+
+    live: "https://ragangithub.github.io/",
+    source: "https://github.com/ragangithub/Microverse-Portfolio",
+  },
+  {
+    image_mobile: "./images/proffesional.png",
+    image_desktop: "./images/Availablity.png",
+    name: " Multi-Post Stories",
+    description:
+      " A daily selection of privately personalized reads; no accounts or sign-ups required.",
+
+    details:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+
+    live: "https://ragangithub.github.io/",
+    source: "https://github.com/ragangithub/Microverse-Portfolio",
+  },
+];
+projects.forEach((project, index) => {
+  const divv = document.createElement("div");
+
+  divv.className = "tonic";
+  divv.id = index;
+  if (index === 1 || index === 3) {
+    divv.classList.add("reverse");
+  }
+  divv.innerHTML = `
+  <div class="mobile">
+    <img src=${project.image_mobile} alt="" />
+  </div>
+  <div class="desktop"><img src=${project.image_desktop} alt="" /></div>
+  <div class="right">
+    <h2>${project.name}</h2>
+    <div class="back">
+      <div>CANOPY</div>
+      <div class="circle"></div>
+      <div>Back End Dev</div>
+      <div class="circle"></div>
+      <div>2015</div>
+    </div>
+    <p>
+    ${project.description}
+    </p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+    <div id='see-project'><a href="">See Project</a></div>
+  </div>
+`;
+
+  works.appendChild(divv);
+});
+const boddy = document.querySelector("body");
+
+document.addEventListener("click", (e) => {
+  const target = e.target.closest("#see-project");
+
+  if (target) {
+    e.preventDefault();
+
+    const idd = target.parentNode.parentNode.id;
+
+    const popup = document.createElement("div");
+    popup.className = "pop";
+    popup.classList.add("tonic");
+    popup.innerHTML = `
+
+<div class="right mobile">
+  <div class="close" >
+   <h2>${projects[idd].name}</h2>
+   <i id="close_pop" class="fa-solid fa-x"></i>
+  
+   
+  </div>
+  <div class="back">
+    <div>CANOPY</div>
+    <div class="circle"></div>
+    <div>Back End Dev</div>
+    <div class="circle"></div>
+    <div>2015</div>
+  </div>
+  <div class="mobile"><img src=${projects[idd].image_mobile} alt="" /></div>
+  <p>
+  ${projects[idd].details}
+  </p>
+  <ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+  </ul>
+  <div class="see">
+   <a class='host' href=${projects[idd].live}>
+    <span>see live</span>
+    <img src="./images/live.svg"/>
+   </a>
+   <a class='git'href=${projects[idd].source}>
+    <span>see source</span>
+    <i class="fa fa-github" aria-hidden="true"></i>
+   </a>
+
+  </div>
+  
+</div>
+
+
+
+<div class="right desktop">
+  <div class="close" >
+   <h2>${projects[idd].name}</h2>
+   <i id="close_pop" class="fa-solid fa-x"></i>
+   
+  </div>
+  <div class="back">
+    <div>CANOPY</div>
+    <div class="circle"></div>
+    <div>Back End Dev</div>
+    <div class="circle"></div>
+    <div>2015</div>
+  </div>
+  <div class="desktop-image"><img src=${projects[idd].image_desktop} alt="" /></div>
+
+
+  <div class=details>
+   
+  <div class='left'>
+  <p>
+  ${projects[idd].details}
+  </p>
+  </div>
+  <div class='right'>
+  <ul >
+  
+     <li>HTML</li>
+     <li>CSS</li>
+     <li>JavaScript</li>
+   
+  </ul>
+
+  <ul class='ruby' >
+  <li>Ruby</li>
+  <li>Bootstrap</li>
+ </ul>
+  <div class="see">
+  <a class='host' href=${projects[idd].live}>
+    <span>see live</span>
+    <img src="./images/live.svg"/>
+   </a>
+   <a class='git' href=${projects[idd].source}>
+    <span>see source</span>
+    <i class="fa fa-github" aria-hidden="true"></i>
+   </a>
+    </div>
+   </div>
+  </div>
+</div>
+`;
+    boddy.style.backgroundColor = "#C1C7D0";
+
+    boddy.style.position = "fixed";
+
+    header.style.display = "none";
+    header.style.backgroundColor = "#C1C7D0";
+
+    popup.classList.add("show");
+    popup.classList.add("tonic");
+    popup.classList.add("pop");
+    popup.classList.remove("hide");
+    boddy.append(popup);
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const popup = document.querySelector(".pop");
+  const target = e.target.closest("#close_pop");
+
+  if (target) {
+    popup.remove();
+
+    boddy.style.position = "static";
+
+    boddy.style.backgroundColor = "#e5e5e5";
+    header.style.display = "block";
+    header.style.backgroundColor = "#ffffff";
+  }
+});
